bc23865f4d663a3a5de18a94b20246cd3795a005
SUCHIAWEI <khjhs100079@gmail.com>
2020-04-11 23:46:20 +0800

    機器人

---
+++ Unity_ML_football/Assets/football-goal/Script/Ball.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public static bool complete;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name== "感應區")
+            {
+            complete = true;
+            }
+    }
+}
